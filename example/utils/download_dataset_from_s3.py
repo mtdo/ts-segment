ea6db2ee9bd2037a0a06076ee0d296f0832d6770
Mihail Douhaniaris <mihail@Mihails-MacBook-Pro.local>
2019-10-18 16:25:42 +0300

    Fixed S3 download path in example

--- example/utils/download_dataset_from_s3.py
+++ example/utils/download_dataset_from_s3.py
@@ -3,4 +3,4 @@ from botocore import UNSIGNED
 from botocore.client import Config
 
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-s3.download_file('mobiactv2', 'MobiAct_Dataset_v2.0.rar', 'data/MobiAct/MobiAct_Dataset_v2.0.rar')
+s3.download_file('mobiactv2', 'MobiAct_Dataset_v2.0.rar', 'data/MobiActV2/MobiAct_Dataset_v2.0.rar')
